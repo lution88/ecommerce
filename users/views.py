347f39c0b6505909e19a30c569db67ce7f74c001
lution88 <lution88@gmail.com>
2022-09-14 10:57:45 +0900

    feat: UserApiView 구현

--- users/views.py
+++ users/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from users.models import User
+from users.serializers import UserSerializer, UserCreateSerializer
+
+
+class UserApiView(APIView):
+    def get(self, request):
+        users = User.objects.all()
+        users_serializer = UserSerializer(users, many=True)
+        return Response(users_serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        user_serializer = UserCreateSerializer(data=request.data)
+        if user_serializer.is_valid():
+            user_serializer.save()
+            return Response(user_serializer.data, status=status.HTTP_200_OK)
+        return Response(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, user_id):
+        user = User.objects.get(id=user_id)
+        user_serializer = UserCreateSerializer(user, data=request.data, partial=True)
+        if user_serializer.is_valid():
+            user_serializer.save()
+            return Response(user_serializer.data, status=status.HTTP_200_OK)
+        return Response(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, user_id):
+        user = User.objects.get(id=user_id)
+        user.delete()
+        return Response({"message": "삭제 완료"}, status=status.HTTP_200_OK)
